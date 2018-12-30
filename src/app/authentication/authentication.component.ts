3c20f715735d014bb95ef1273dc3c00ed872585a
neha-bharti1212 <neha.bharti@meltwater.com>
2018-12-30 21:26:08 +0530

    Updated Login Code

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import { FormBuilder, FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-authentication',
